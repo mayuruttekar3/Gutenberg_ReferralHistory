1329de2df7643cef3464a47778a6391a954fbf43
Mayur U <mayuruttekar3@gmail.com>
2025-04-29 18:50:33 +0530

    Custom plugins to create gutenberg blocks

---
+++ wp-content/plugins/custom-sql/queries.php
@@ -0,0 +1,26 @@
+<?php
+// Which query would you write to find every duplicate in a table? You should mention possible methods.
+
+Answer 1 : We can possiblty write two method
+
+1) Using GROUP BY and HAVING clause
+
+SELECT column, COUNT(*) as no_count FROM table_name
+GROUP BY column
+HAVING COUNT(*) > 1;
+
+Example : 
+
+SELECT first_name, COUNT(*) as no_of_first FROM wp_users
+GROUP BY first_name
+HAVING COUNT(*) > 1;
+
+2) Using Self Join : Join table to it self. basically for duplicated entry we used it if you dont work with wordpress 
+
+Example:
+
+SELECT a.* FROM wp_users a
+JOIN wp_users b ON a.email = b.email AND a.id < b.id;
+
+This query output the duplicate emails of same wp_users table
+
